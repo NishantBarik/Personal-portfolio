c36e093630856560020b9d113fac3de0b68c541b
Nishant Barik <bariknishant10@gmail.com>
2025-06-12 20:30:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".multiple-text",{
+    strings:["Engineer.","Game Developer.","Student.","FrontEnd Developer.","Gamer."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
